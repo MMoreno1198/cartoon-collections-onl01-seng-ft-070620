64c29634fd0c6444b0b15f75463c6dca1b825086
Marcus Moreno <legendary.1198@gmail.com>
2020-07-13 23:41:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,19 +1,20 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
-end
-require 'pry'
+def roll_call_dwarves
 
-def summon_captain_planet# code an argument here
-  # Your code here
+end
+
+def summon_captain_planet
+ 
 def roll_call_dwarves(array)
+  
   array.each_with_index do |name, index|
+    
     puts "#{index + 1} #{name}"
+    
   end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls
 
-  # Your code here
 def summon_captain_planet(array)
   
   array.map do |x|
@@ -23,11 +24,10 @@ def summon_captain_planet(array)
   end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese
 
-  # the array below is here to help
-  
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
 def long_planeteer_calls(array)
   
   array.any? do |word|
